d3d924878612a2ddbc3d3df672784a5d7d93d5c5
Jini Byun <jinibyun@gmail.com>
2017-10-25 23:08:22 -0400

    commit

---
+++ CSharpWithPython/PythonApplication1/dataTypeString.py
@@ -0,0 +1,15 @@
+# 5 ways to make string
+print("Hello World")
+print("""Hello World""")
+print('Hello World')
+print('''Hello World''')
+print("""Hello World""")
+
+multiline = "Life is too short\nYou need python"
+print(multiline)
+
+multiline = '''
+Life is too short
+You need python
+'''
+print (multiline)
